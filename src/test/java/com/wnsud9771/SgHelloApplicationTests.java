52240e9f6f52ae66f8b49be2ab7e6235cac6aaeb
jun9771 <jun97718@gmail.com>
2024-09-26 13:34:10 +0900

    sg-hello first commit

---
+++ src/test/java/com/wnsud9771/SgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnsud9771;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
